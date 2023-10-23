f9d0c8cafe3e351d75b3e13aa0328d9a4d01b011
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2023-10-24 02:04:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const port =process.env.PORT || 3000;
 const _ = require("lodash")
 
 const app = express();
@@ -109,6 +110,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
